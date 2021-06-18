4551e14466fd1746d8f96524a26acc134745a952
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2021-06-18 20:30:37 -0300

    Java 8+, java.time

---
+++ src/trabalhandoComDate/DateFormat/Exemplo005.java
@@ -0,0 +1,15 @@
+package trabalhandoComDate.DateFormat;
+
+import java.time.LocalDate;
+
+public class Exemplo005 {
+
+    public static void main(String[] args) {
+
+
+    LocalDate hoje = LocalDate.now();
+
+    System.out.println(hoje);
+    //2021-06-18
+    }
+}
