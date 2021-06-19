7f634b0aa11219a38ab7e9f4c42d619e02a4974b
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2021-06-19 18:52:47 -0300

    Trabalhando com Arrays

---
+++ src/trabalhandoComArrays/MyArrays.java
@@ -0,0 +1,11 @@
+package trabalhandoComArrays;
+
+public class MyArrays {
+    public static void main(String[] args) {
+
+        int[] meuArray = {12,32,33,22,55,43,54};
+                for(int i=0; i<7; i++) {
+                    System.out.println(meuArray[i]);
+                }
+    }
+}
